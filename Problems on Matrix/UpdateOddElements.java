55a5dddf82e811710f7795620bd221636878ac6c
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-24 14:56:36 +0530

    Create UpdateOddElements.java

---
+++ Problems on Matrix/UpdateOddElements.java
@@ -0,0 +1,93 @@
+/*
+    Program to accept number of rows and columns from user accept that much elements from user and display Matrix and update the odd elements by increasing 1
+*/
+
+import java.util.*;
+
+class Matrix
+{
+public int iRow;
+public int iCol;
+public int Arr[][];
+
+    public Matrix(int A, int B)
+    {
+        this.iRow = A;
+        this.iCol = B;
+        Arr = new int[iRow][iCol];
+    }
+
+    public void Accept()
+    {
+        Scanner sobj = new Scanner(System.in);
+        int i = 0, j = 0;
+
+        System.out.println("Please enter the data : ");
+        for(i = 0; i < iRow; i++)
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+    }
+
+    public void Display()
+    {
+        int i = 0;
+        int j = 0;
+
+        for(i = 0; i < iRow; i++)
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                System.out.print(Arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void UpdateMatrixEven()
+    {
+        int  i = 0, j = 0;
+        
+        for(i= 0; i < iRow; i++)
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                if(Arr[i][j] % 2 == 1)
+                {
+                    Arr[i][j]++;
+                }
+            }
+        }
+    }
+}
+
+class asd
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows : ");
+        int No1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        int No2 = sobj.nextInt();
+
+        Matrix mobj = new Matrix(No1, No2);
+
+        mobj.Accept();
+        System.out.println("Original Matrix :");
+        mobj.Display();
+
+        mobj.UpdateMatrixEven();
+        System.out.println("Updated Matrix :");
+        mobj.Display();
+
+        mobj = null;
+
+        sobj.close();
+    }
+}
