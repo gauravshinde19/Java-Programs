4cf030a00131f1e477298727c14eb7dbde2d9a3b
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-24 15:21:27 +0530

    Create DisplayString.java

---
+++ Strings/DisplayString.java
@@ -0,0 +1,28 @@
+/*
+    Program to accept string from user and print that string
+*/
+
+import java.util.*;
+
+class Strings
+{
+    public static void Display(String str)
+    {
+        System.out.println("Entered string : " + str);
+    }
+}
+
+class asd
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the string : ");
+        String str = sobj.nextLine();
+
+        Strings.Display(str);
+
+        sobj.close();
+    }
+}
