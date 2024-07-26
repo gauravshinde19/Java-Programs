d8545f8d3d1730bf695f372f1971073b7d129901
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-26 15:37:41 +0530

    Create LargestWordAndLength.java

---
+++ Strings/LargestWordAndLength.java
@@ -0,0 +1,38 @@
+/*
+  Program to accept string from the user and find the largest word and its length
+*/
+
+import java.util.*;
+
+class LargestWordAndLength
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter string : ");
+        String str = sobj.nextLine();
+
+        str = str.trim();
+        
+        str = str.replaceAll("\\s+"," ");
+
+        String Arr[] = str.split(" ");
+        
+        int i = 0;
+        int iMax = 0;
+        String MaxStr = null;
+
+        for(i = 0 ; i < Arr.length; i++)
+        {
+            if(Arr[i].length() > iMax)
+            {
+                iMax = Arr[i].length();
+                MaxStr = Arr[i];
+            }
+        }
+
+        System.out.println("Largest word is : "+MaxStr);
+        System.out.println("Length of largest word is : "+iMax);
+    }
+}
