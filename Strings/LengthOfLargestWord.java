8ea754799d58b9f87be571f5384b4520cfb75db1
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-26 15:32:40 +0530

    Create LengthOfLargestWord.java

---
+++ Strings/LengthOfLargestWord.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class LengthOfLargestWord
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter string : ");
+        String str = sobj.nextLine();
+
+        str = str.trim();
+        
+        str = str.replaceAll("\\s+"," ");
+
+        String Arr[] = str.split(" ");
+        
+        int i = 0;
+        int iMax = 0;
+
+        for(i = 0 ; i < Arr.length; i++)
+        {
+            if(Arr[i].length() > iMax)
+            {
+                iMax = Arr[i].length();
+            }
+        }
+
+        System.out.println("Length of largest word is : " + iMax);
+    }
+}
