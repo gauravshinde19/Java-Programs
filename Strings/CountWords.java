0d9aae0c85ac1d3e601f332bcd07b7fd3818ad2b
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-26 15:22:30 +0530

    Update CountWords.java
    update to simple way

--- Strings/CountWords.java
+++ Strings/CountWords.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class CountWords
 {
-    public static void main(String Arg[])
+   public static void main(String Arg[])
     {
         Scanner sobj = new Scanner(System.in);
 
@@ -10,19 +10,11 @@ class CountWords
         String str = sobj.nextLine();
 
         str = str.trim();
+        
         str = str.replaceAll("\\s+"," ");
 
-        char Arr[] = str.toCharArray();
+        String Arr[] = str.split(" ");
 
-        int iCnt = 0, i = 0;
-
-        for(i = 0; i < Arr.length; i++)
-        {
-            if(Arr[i] ==' ')
-            {
-                iCnt++;
-            }
-        }
-        System.out.println("Number of words are : " + (iCnt+1));
+        System.out.println("Number of words are : "+Arr.length);
     }
 }
