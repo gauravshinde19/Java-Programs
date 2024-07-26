b31a4d929ed5c59c2569e1942d8370b4395b74e1
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-07-26 17:02:41 +0530

    Update DisplayWords.java
    updated to OOP approach

--- Strings/DisplayWords.java
+++ Strings/DisplayWords.java
@@ -1,6 +1,26 @@
-// Program to accept string from user and display all the words from given string
+/*
+    Program to accept string from user and display all the words from given string
+*/
 
 import java.util.*;
+
+class Strings
+{
+    public static void DisplayWords(String str)
+    {
+        str = str.trim();
+        
+        str = str.replaceAll("\\s+"," ");
+
+        String Arr[] = str.split(" ");
+
+        System.out.println("Words from the string are : ");
+        for(String s : Arr)
+        {    
+            System.out.println(s);
+        }
+    }
+}
 
 class DisplayWords
 {
@@ -8,21 +28,11 @@ class DisplayWords
     {
         Scanner sobj = new Scanner(System.in);
 
-        System.out.println("Enter string : ");
+        System.out.println("Enter the string : ");
         String str = sobj.nextLine();
 
-        str = str.trim();
+        Strings.DisplayWords(str);
         
-        str = str.replaceAll("\\s+"," ");
-
-        String Arr[] = str.split(" ");
-
-        int i = 0;
-
-        System.out.println("Words from the string are : ");
-        for(String s : Arr)
-        {    
-            System.out.println(s);
-        }
+        sobj.close();
     }
 }
